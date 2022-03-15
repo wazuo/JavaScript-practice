51a106abc63a394560bc671bf6f931c894a963ce
wazuo <95359918+wazuo@users.noreply.github.com>
2022-03-15 14:06:21 +0900

    [end]javascript2章

---
+++ 2章/app.js
@@ -0,0 +1,34 @@
+let hello = 'Hello World';
+alert(hello);
+
+let orange =120
+let apple = 100
+
+if(orange < apple){
+  alert('みかんの値段がリンゴより安い');
+}else if(orange == apple){
+  alert('みかんとリンゴが同じ値段');
+}else{
+  alert('みかんの値段がリンゴより高い');
+}
+
+
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
+
+let m = 3 + 1;
+let s = 6 - 1;
+let t = 4 * 3;
+let d = 15 / 3;
+
+alert('3 + 1は' + m + 'です');
+alert ('6 - 1は' + s + 'です');
+alert('4 * 3は' + t + 'です')
+alert('15 / 3 は' + d + 'です')
+
